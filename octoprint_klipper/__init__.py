d4fb679067746a609792dfa464e6364680878a2c
Jacopo Tediosi <jacopotediosi@gmail.com>
2026-05-16 12:21:33 +0200

    implement is_api_protected
    Ref: https://docs.octoprint.org/en/dev/plugins/mixins.html#octoprint.plugin.SimpleApiPlugin.is_api_protected

--- octoprint_klipper/__init__.py
+++ octoprint_klipper/__init__.py
@@ -386,6 +386,9 @@ class KlipperPlugin(
             log_info(self, self._message)
             self._message = ""
 
+    def is_api_protected(self):
+        return True
+
     def get_api_commands(self):
         return dict(
             listLogFiles=[],
